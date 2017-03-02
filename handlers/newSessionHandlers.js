cf08fd277d94892fa15f4638a38d50f9e5d94717
Brent Pabst <bpabst@bpabst-macpro.local>
2017-03-01 23:29:01 -0500

    More documentation

--- handlers/newSessionHandlers.js
+++ handlers/newSessionHandlers.js
@@ -2,22 +2,27 @@ const constants = require('../constants');
 
 const newSessionHandlers = {
   LaunchRequest() {
+    // All default requests to the skill
     this.handler.state = constants.states.START;
     this.emitWithState('LaunchRequest', true);
   },
   PlayStream() {
+    // Play the stream
     this.handler.state = constants.states.STREAM;
     this.emitWithState('PlayStream', true);
   },
   CurrentSong() {
+    // Get the current song
     this.handler.state = constants.states.START;
     this.emitWithState('CurrentSong', true);
   },
   RequestSong() {
+    // Request a song
     this.handler.state = constants.states.REQUEST;
     this.emitWithState('RequestSong', true);
   },
   Unhandled() {
+    // Not sure what to do
     this.emit(':ask', 'Unhandled', 'Unhandled');
   },
 };
